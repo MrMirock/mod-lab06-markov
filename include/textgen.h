3b60b60b4e6f3824d387b92dec856a9269813c06
Mirock <kashingeotgii@gmail.com>
2026-05-04 16:25:10 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -11,16 +11,16 @@ typedef std::deque<std::string> prefix;
 typedef std::vector<std::string> suffix;
 
 class MarkovTextGenerator {
-public:
-	explicit MarkovTextGenerator(const std::string& text, int nPref = 2);
-	explicit MarkovTextGenerator(std::map<prefix, suffix> table);
+ public:
+  explicit MarkovTextGenerator(const std::string& text, int nPref = 2);
+  explicit MarkovTextGenerator(std::map<prefix, suffix> table);
 
-	std::string generate(int maxLen);
+  std::string generate(int maxLen);
 
-	const std::map<prefix, suffix>& getTable() const { return table; }
-	const prefix& getFirstPrefix() const { return firstPrefix; }
+  const std::map<prefix, suffix>& getTable() const { return table; }
+  const prefix& getFirstPrefix() const { return firstPrefix; }
 
-private:
-	std::map<prefix, suffix> table;
-	prefix firstPrefix;
+ private:
+  std::map<prefix, suffix> table;
+  prefix firstPrefix;
 };
