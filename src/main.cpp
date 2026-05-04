8237e7f4b6c5f36ce22a95ad464813e940013515
Mirock <kashingeotgii@gmail.com>
2026-05-04 16:11:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
+// Copyright 2026 Kashin Georgii
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "../include/textgen.h"  // путь к заголовку
+#include <string>
+#include "textgen.h"
 
 int main() {
-    std::ifstream file("C:\Users\Tolya\source\repos\mod-lab06-markov\input\input.txt");
+    std::ifstream file("input.txt");
     if (!file) {
         std::cerr << "Error opening input.txt\n";
         return 1;
     }
+
     std::stringstream buffer;
     buffer << file.rdbuf();
     std::string text = buffer.str();
@@ -16,15 +20,14 @@ int main() {
     MarkovTextGenerator gen(text, 2);
     std::string result = gen.generate(1500);
 
-    // Сохраняем в result/gen.txt
-    std::ofstream out("C:\Users\Tolya\source\repos\mod-lab06-markov\result\gen.txt");
+    std::ofstream out("output.txt");
     if (!out) {
-        std::cerr << "Error creating gen.txt\n";
+        std::cerr << "Error creating output.txt\n";
         return 1;
     }
     out << result;
     out.close();
 
-    std::cout << "Text saved to result/gen.txt\n";
+    std::cout << "Text saved to output.txt\n";
     return 0;
 }
